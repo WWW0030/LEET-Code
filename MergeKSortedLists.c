a1515b64a30c1d35f8794173e7f0ffa983adfc7f
WWW0030 <howe.wang0030@gmail.com>
2022-04-27 10:44:14 -0400

    MergeKSortedLists

---
+++ MergeKSortedLists.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* mergeTwoLists(struct ListNode *alist, struct ListNode *blist) {
+    struct ListNode *aPointer = alist;
+    struct ListNode *bPointer = blist;
+    if(aPointer == NULL && bPointer == NULL) {
+        return NULL;
+    }
+    else if(aPointer == NULL) {
+        return bPointer;
+    }
+    else if (bPointer == NULL) {
+        return aPointer;
+    }
+    struct ListNode *ans = NULL;
+    if((*aPointer).val > (*bPointer).val) {
+        ans = bPointer;
+        bPointer = (*bPointer).next;
+    }
+    else {
+        ans = aPointer;
+        aPointer = (*aPointer).next;
+    }
+    struct ListNode *ansPointer = ans;
+    while(aPointer != NULL && bPointer != NULL) {
+        int a = (*aPointer).val;
+        int b = (*bPointer).val;
+        if (a > b) {
+            (*ansPointer).next = bPointer;
+            bPointer = (*bPointer).next;
+            ansPointer = (*ansPointer).next;
+        }
+        else {
+            (*ansPointer).next = aPointer;
+            aPointer = (*aPointer).next;
+            ansPointer = (*ansPointer).next;
+        }
+    }
+    if (aPointer == NULL) {
+        (*ansPointer).next = bPointer;
+    }
+    else if (bPointer == NULL) {
+        (*ansPointer).next = aPointer;
+    }
+    return ans;
+}
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize){
+    if(listsSize == 0) return NULL;
+    if(listsSize == 1) return lists[0];
+    struct ListNode *ans = lists[0];
+    for(int i = 1; i < listsSize; ++i) {
+        ans = mergeTwoLists(ans, lists[i]);
+    }
+    return ans;
+}
